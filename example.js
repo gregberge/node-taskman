38bf1fb4605a3a6a0b22f096fedc0dcda68a6c99
Greg Bergé <neoziro@gmail.com>
2012-04-27 12:31:09 +0200

    use multi for performance

--- example.js
+++ example.js
@@ -7,8 +7,8 @@ var test = function(data, callback)
    callback();
 };
 
-var config = {port: 6379, host: "arnold.lemonde-interactif.fr", db: 3};
+var config = {port: 6379, host: "localhost", db: 3};
 var driver = new RedisDriver(config);
-var options = {waitTime : 100, sleepTime: 30};
+var options = {waitTime : 100, sleepTime: 0, multiNumber: 100};
 var worker = new Worker("notify:apn", test, driver, options);
 worker.start();
