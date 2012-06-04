0520ed5f0e14553c38d5fde567f6cb2145baa493
Greg Bergé <berge@lemonde.fr>
2012-06-04 18:50:45 +0200

    update example + corrections

--- example.js
+++ example.js
@@ -1,33 +1,13 @@
-var node_taskman = require("./index");
+var taskman = require("./index"), driver, queue, worker;
 
-var config = {db: 3};
+driver = new taskman.driver.RedisDriver();
+queue = new taskman.Queue("test_queue", driver);
 
-var driver = new node_taskman.driver.RedisDriver(config);
+worker = new taskman.Worker(queue, driver, "test_worker", function(data, callback){
+   console.log(data);
+   callback();
+});
 
-var queue = new node_taskman.Queue("test", driver);
-
-var action = function(data, callback){console.log(data); callback();};
-
-var worker = new node_taskman.Worker(queue, driver, "test_worker", action);
-
-var cb = function()
-{
-   console.log("start ok");
-};
-
-worker.start(cb);
-
-
-
-//var queue = new node_taskman.Queue();
-
-/*
-var Worker = require("./lib/index").Worker,
-    RedisDriver = require('./lib/driver/redis').RedisDriver;
-
-var config = {port: 6379, host: "localhost", db: 3};
-var driver = new RedisDriver(config);
-var options = {waitTime : 100, sleepTime: 1000, multiNumber: 2};
-var worker = new Worker("notify:apn", test, driver, options);
-worker.start();
-*/
+queue.rpush("Hello World", function(){
+   worker.start();
+});
