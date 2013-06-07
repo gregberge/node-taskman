87b6848f528699d2249137345ed3fd8fb4791b5a
Greg Bergé <neoziro@gmail.com>
2013-06-07 14:08:48 +0200

    fix code, add memory driver

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.Worker = require('./lib/worker').Worker;
 exports.Queue = require('./lib/queue').Queue;
 exports.driver = {
-  RedisDriver: require('./lib/driver/redis').RedisDriver
+  RedisDriver: require('./lib/driver/redis').RedisDriver,
+  MemoryDriver: require('./lib/driver/memory').MemoryDriver
 };
